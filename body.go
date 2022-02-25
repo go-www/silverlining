fdd277622786970ef116486dd7676d342b87708b
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-25 13:28:10 +0900

    (*Context).Body()

---
+++ body.go
@@ -0,0 +1,19 @@
+package silverlining
+
+import (
+	"errors"
+	"io"
+)
+
+var ErrBodyTooLarge = errors.New("body too large")
+
+func (r *Context) Body() ([]byte, error) {
+	if r.reqR.Request.ContentLength > r.server.MaxBodySize {
+		return nil, ErrBodyTooLarge
+	}
+
+	var buf []byte = make([]byte, r.reqR.Request.ContentLength)
+	_, err := io.ReadAtLeast(r.BodyReader(), buf, len(buf))
+	r.CloseBodyReader()
+	return buf, err
+}
