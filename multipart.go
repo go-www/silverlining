bcfd364c8a4ca6030d278abddd13898c4699c1c0
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-27 11:42:25 +0900

    (*Context).MultipartReader()

---
+++ multipart.go
@@ -0,0 +1,25 @@
+package silverlining
+
+import (
+	"errors"
+	"mime"
+	"mime/multipart"
+	"strings"
+)
+
+var ErrContentTypeInvalid = errors.New("content-type invalid")
+
+func (r *Context) MultipartReader() (*multipart.Reader, error) {
+	contentType, ok := r.RequestHeaders().Get("Content-Type")
+	if !ok {
+		return nil, ErrContentTypeInvalid
+	}
+	mediatype, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasPrefix(mediatype, "multipart/") {
+		return nil, ErrContentTypeInvalid
+	}
+	return multipart.NewReader(r.BodyReader(), params["boundary"]), nil
+}
