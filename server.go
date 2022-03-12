d0bf734c57da880287bedd3e202dda79a07dec0b
lemon-mint <ice.lemons.mint@gmail.com>
2022-03-12 15:10:16 +0900

    ListenAndServePrefork

--- server.go
+++ server.go
@@ -2,6 +2,9 @@ package silverlining
 
 import (
 	"net"
+	"os"
+	"os/exec"
+	"runtime"
 
 	reuse "github.com/libp2p/go-reuseport"
 )
@@ -32,4 +35,31 @@ func ListenAndServeReusePort(addr string, handler Handler) error {
 	}
 
 	return srv.Serve(ln)
+}
+
+func ListenAndServePrefork(addr string, handler Handler) error {
+	runtime.GOMAXPROCS(1)
+
+	IsChildEnv := os.Getenv("SILVERLINING_PREFORK_CHILD")
+	IsChild := IsChildEnv == "1"
+
+	if !IsChild {
+		numCPU := runtime.NumCPU()
+		var env []string
+		env = append(env, os.Environ()...)
+		env = append(env, "GOMAXPROCS=1", "SILVERLINING_PREFORK_CHILD=1")
+
+		for i := 0; i < numCPU; i++ {
+			cmd := exec.Command(os.Args[0], os.Args[1:]...)
+			cmd.Env = env
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			if err := cmd.Start(); err != nil {
+				return err
+			}
+		}
+	}
+
+	return ListenAndServeReusePort(addr, handler)
 }
